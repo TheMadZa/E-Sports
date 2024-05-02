9628115a3c1a206c8f02eb1ca03ca763887e77e1
ZahirRiveraChacon <161701860+ZahirRiveraChacon@users.noreply.github.com>
2024-05-02 13:00:40 +0200

    Update Triggers.sql
    Agregado trigger para clasificacion

--- Base de datos/Fase 2/Triggers.sql
+++ Base de datos/Fase 2/Triggers.sql
@@ -4,6 +4,7 @@ DROP TRIGGER GENERAR_CALENDARIO;
 DROP TRIGGER NOMODIFICAR_EQUIPO;
 DROP TRIGGER NOMODIFICAR_JUGADOR;
 DROP TRIGGER MAXSALARIO_EQUIPO;
+DROP TRIGGER actualizar_resultados;
 
 /*Controlar que no haya más de 6 ni menos de 2 jugadores en un equipo.*/
 CREATE OR REPLACE TRIGGER CANTIDAD_EQUIPO
@@ -127,4 +128,35 @@ BEGIN
 
 END MAXSALARIO_EQUIPO;
 
+CREATE OR REPLACE TRIGGER actualizar_resultados
+AFTER INSERT ON ENFRENTAMIENTO
+FOR EACH ROW
+DECLARE
+    v_equipo1 NUMBER(2);
+    v_equipo2 NUMBER(2);
+    v_resultado1 NUMBER(2);
+    v_resultado2 NUMBER(2);
+BEGIN
+    -- Obtener los datos del enfrentamiento insertado
+    v_equipo1 := :NEW.ID_EQUIPO1;
+    v_equipo2 := :NEW.ID_EQUIPO2;
+    v_resultado1 := :NEW.RESULTADO1;
+    v_resultado2 := :NEW.RESULTADO2;
+    
+    -- Actualizar los puntos del equipo 1 y 2 sumando el resultado obtenido
+    UPDATE EQUIPO_COMPETICION
+    SET PUNTOS = PUNTOS + v_resultado1
+    WHERE ID_EQUIPO IN (v_equipo1, v_equipo2);
 
+    -- Si el equipo 1 ganó, se le suma una victoria
+    IF v_resultado1 > v_resultado2 THEN
+        UPDATE EQUIPO_COMPETICION
+        SET VICTORIAS = VICTORIAS + 1
+        WHERE ID_EQUIPO = v_equipo1;
+    -- Si el equipo 2 ganó, se le suma una victoria
+    ELSIF v_resultado1 < v_resultado2 THEN
+        UPDATE EQUIPO_COMPETICION
+        SET VICTORIAS = VICTORIAS + 1
+        WHERE ID_EQUIPO = v_equipo2;
+    END IF;
+END;
