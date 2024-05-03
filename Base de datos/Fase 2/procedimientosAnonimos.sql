8791e96bce92c51790e9c0d56c1684fddc4d792d
Lorena-Arbe-Sanchez <126677607+Lorena-Arbe-Sanchez@users.noreply.github.com>
2024-05-03 10:52:45 +0200

    Añadir a procedimientosAnonimos.sql más pruebas
    Añadir a procedimientosAnonimos.sql las pruebas de los procedimientos que están en el paquete.

--- Base de datos/Fase 2/procedimientosAnonimos.sql
+++ Base de datos/Fase 2/procedimientosAnonimos.sql
@@ -1,12 +1,14 @@
 SET SERVEROUTPUT ON;
 
+-- Pruebas de los procedimientos almacenados que est√°n en scripts separados:
+
 /*
 Prueba del procedimiento almacenado "OBTENER_INFO_COMPETICION".
 
-Muestra datos sobre la competiciÛn y su juego, los enfrentamientos jugados,
+Muestra datos sobre la competici√≥n y su juego, los enfrentamientos jugados,
 y la cantidad de jugadores y de staff de cada equipo.
 
-Se utilizar· como consulta en la ventana del administrador para visualizar las
+Se utilizar√° como consulta en la ventana del administrador para visualizar las
 competiciones con sus respectivos datos.
 */
 DECLARE
@@ -53,8 +55,8 @@ Prueba del procedimiento almacenado "OBTENER_INFORMACION".
 
 Muestra el nombre de cada equipo con la cantidad de jugadores y staff que tiene.
 
-Se utilizar· como consulta en la ventana de un usuario corriente para visualizar
-los equipos con el n˙mero total de jugadores y de staff.
+Se utilizar√° como consulta en la ventana de un usuario corriente para visualizar
+los equipos con el n√∫mero total de jugadores y de staff.
 */
 DECLARE
   c_obtenerInfo SYS_REFCURSOR;
@@ -63,6 +65,67 @@ DECLARE
   cantidadJugadores NUMBER;
 BEGIN
   OBTENER_INFORMACION(c_obtenerInfo);
+  LOOP
+    FETCH c_obtenerInfo INTO
+      nom_equipo, 
+      cantidadStaff,
+      cantidadJugadores;
+    EXIT WHEN c_obtenerInfo%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE('Nombre del Equipo: ' || nom_equipo ||
+    ', Cantidad de Staff: ' || cantidadStaff ||
+    ', Cantidad de jugadores: ' || cantidadJugadores);
+  END LOOP;
+  CLOSE c_obtenerInfo;
+END;
+
+-- Pruebas de los procedimientos almacenados que est√°n en el paquete "PAQUETE_DATOS":
+
+DECLARE
+  c_obtenerInfoCompeticion SYS_REFCURSOR;
+  nombreCom COMPETICION.NOMBRE_COM%TYPE;
+  nombre JUEGO.nombre%TYPE;
+  hora_enfrentamiento ENFRENTAMIENTO.hora_enfrentamiento%TYPE;
+  equipo_1 EQUIPO.nom_equipo%TYPE;
+  cantidad_jugadores_equipo_1 NUMBER;
+  cantidad_staff_equipo_1 NUMBER;
+  equipo_2 EQUIPO.nom_equipo%TYPE;
+  cantidad_jugadores_equipo_2 NUMBER;
+  cantidad_staff_equipo_2 NUMBER;
+BEGIN
+  PAQUETE_DATOS.OBTENER_INFO_COMPETICION(c_obtenerInfoCompeticion);
+  LOOP
+    FETCH c_obtenerInfoCompeticion INTO
+      nombreCom,
+      nombre,
+      hora_enfrentamiento,
+      equipo_1,
+      cantidad_jugadores_equipo_1,
+      cantidad_staff_equipo_1,
+      equipo_2,
+      cantidad_jugadores_equipo_2,
+      cantidad_staff_equipo_2;
+    EXIT WHEN c_obtenerInfoCompeticion%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE('Nombre Competicion: ' || nombreCom || '
+    Nombre del Juego: ' || nombre || '
+    Fecha y hora: ' || TO_CHAR(hora_enfrentamiento,'DD-MM-YY HH24:MI') ||'
+    Equipo 1: ' || equipo_1 ||'
+    Cantidad de jugadores del Equipo 1: ' || cantidad_jugadores_equipo_1 ||'
+    Cantidad de staff del Equipo 1: ' || cantidad_staff_equipo_1 ||'
+    Equipo 2: ' || equipo_2 ||'
+    Cantidad de jugadores del Equipo 2: ' || cantidad_jugadores_equipo_2 ||'
+    Cantidad de staff del Equipo 2: ' || cantidad_staff_equipo_2 ||'
+    ');
+  END LOOP;
+  CLOSE c_obtenerInfoCompeticion;
+END;
+
+DECLARE
+  c_obtenerInfo SYS_REFCURSOR;
+  nom_equipo EQUIPO.nom_equipo%TYPE;
+  cantidadStaff NUMBER;
+  cantidadJugadores NUMBER;
+BEGIN
+  PAQUETE_DATOS.OBTENER_INFORMACION(c_obtenerInfo);
   LOOP
     FETCH c_obtenerInfo INTO
       nom_equipo, 
