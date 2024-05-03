07a13db7b1e8a2d67d0504b2a4c55809d4bd8ab8
ZahirRiveraChacon <161701860+ZahirRiveraChacon@users.noreply.github.com>
2024-05-03 10:02:52 +0200

    Prueba para la función del equipo
    Prueba en la que encontramos el nombre del líder del equipo usando la función

---
+++ Base de datos/Fase 2/PruebaDeFuncion.sql
@@ -0,0 +1,15 @@
+DECLARE
+    resultado VARCHAR2(200);
+BEGIN
+    -- Probar la función con un equipo que tiene un líder
+    resultado := FUNCION_LIDER('2');
+    DBMS_OUTPUT.PUT_LINE(resultado);
+
+    -- Probar la función con un equipo que no tiene líder
+    --resultado := FUNCION_LIDER('');
+    --DBMS_OUTPUT.PUT_LINE(resultado);
+
+    -- Probar la función con un equipo que no existe
+    resultado := FUNCION_LIDER('31');
+    DBMS_OUTPUT.PUT_LINE(resultado);
+END;
