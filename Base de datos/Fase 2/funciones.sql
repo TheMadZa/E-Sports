ed993074ad8b15aae2e83175b46f6747d079476c
Lorena-Arbe-Sanchez <126677607+Lorena-Arbe-Sanchez@users.noreply.github.com>
2024-05-03 14:03:58 +0200

    Update funciones.sql

--- Base de datos/Fase 2/funciones.sql
+++ Base de datos/Fase 2/funciones.sql
@@ -1,23 +1,2 @@
-/*
-Funcion para obtener el nombre de un equipo y su lider, pasandole como
-parametro el id del equipo.
-*/
-CREATE OR REPLACE FUNCTION FUNCION_LIDER(p_id_equipo IN equipo.id_equipo%type)
-RETURN VARCHAR2
-IS
-    nombre_lider VARCHAR2(50);
-    nombre_equipo VARCHAR2(50);
-BEGIN
-    SELECT J.NOMBRE, E.NOM_EQUIPO
-    INTO nombre_lider, nombre_equipo
-    FROM JUGADOR J
-    JOIN EQUIPO E ON J.ID_EQUIPO = E.ID_EQUIPO
-    WHERE J.ID_EQUIPO = p_id_equipo AND lower(J.ROL) = 'lider';
-
-    RETURN 'El lider del equipo ' || nombre_equipo || ' es ' || InitCap(nombre_lider) || '.';
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        RETURN 'No se encontro un lider para el equipo especificado.';
-    WHEN OTHERS THEN
-        RETURN 'Ocurrio un error inesperado.';
-END FUNCION_LIDER;
+-- Función para que obtenga el resultado de un count de "JUGADOR"
+--  desde el dato de entrada "ID_EQUIPO".
