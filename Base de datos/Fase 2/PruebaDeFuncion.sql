6bdf1cc046182dbe50fb8c3c38e2764cf3db9158
Lorena-Arbe-Sanchez <126677607+Lorena-Arbe-Sanchez@users.noreply.github.com>
2024-05-03 10:54:43 +0200

    Añadir a PruebaDeFuncion.sql más pruebas
    Añadir a PruebaDeFuncion.sql la prueba de la función que está en el paquete.

--- Base de datos/Fase 2/PruebaDeFuncion.sql
+++ Base de datos/Fase 2/PruebaDeFuncion.sql
@@ -1,3 +1,5 @@
+-- Prueba de la función que está en un script:
+
 DECLARE
     resultado VARCHAR2(200);
 BEGIN
@@ -11,5 +13,23 @@ BEGIN
 
     -- Probar la función con un equipo que no existe
     resultado := FUNCION_LIDER(31);
+    DBMS_OUTPUT.PUT_LINE(resultado);
+END;
+
+-- Prueba de la función que está en el paquete "PAQUETE_DATOS":
+
+DECLARE
+    resultado VARCHAR2(200);
+BEGIN
+    -- Probar la función con un equipo que tiene un líder
+    resultado := PAQUETE_DATOS.FUNCION_LIDER(2);
+    DBMS_OUTPUT.PUT_LINE(resultado);
+
+    -- Probar la función con un equipo que no tiene líder
+    --resultado := PAQUETE_DATOS.FUNCION_LIDER();
+    --DBMS_OUTPUT.PUT_LINE(resultado);
+
+    -- Probar la función con un equipo que no existe
+    resultado := PAQUETE_DATOS.FUNCION_LIDER(31);
     DBMS_OUTPUT.PUT_LINE(resultado);
 END;
