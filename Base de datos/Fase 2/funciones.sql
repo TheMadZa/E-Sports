9b9b9add247f4131a2c2055b1ccd5afd71d4ecfa
ZahirRiveraChacon <161701860+ZahirRiveraChacon@users.noreply.github.com>
2024-05-03 10:02:04 +0200

    Funcion LiderEquipo.sql
    función para poder encontrar el líder de un equipo

--- Base de datos/Fase 2/funciones.sql
+++ Base de datos/Fase 2/funciones.sql
@@ -1,11 +1,23 @@
 /*
-Función para obtener el nombre del líder de un equipo, pasándole como
-parámetro el nombre del equipo.
+Funcion para obtener el nombre del lider de un equipo, pasandole como
+parametro el id del equipo.
 */
-CREATE OR REPLACE FUNCTION FUNCION_LIDER ()
-RETURN X
+CREATE OR REPLACE FUNCTION FUNCION_LIDER(p_id_equipo IN equipo.id_equipo%type)
+RETURN VARCHAR2
 IS
-	;
+    nombre_lider VARCHAR2(50);
+    nombre_equipo VARCHAR2(50);
 BEGIN
-	;
-END FUNCION_;
+    SELECT J.NOMBRE, E.NOM_EQUIPO
+    INTO nombre_lider, nombre_equipo
+    FROM JUGADOR J
+    JOIN EQUIPO E ON J.ID_EQUIPO = E.ID_EQUIPO
+    WHERE J.ID_EQUIPO = p_id_equipo AND lower(J.ROL) = 'lider';
+
+    RETURN 'El líder del equipo ' || nombre_equipo || ' es ' || nombre_lider || '.';
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        RETURN 'No se encontró un líder para el equipo especificado.';
+    WHEN OTHERS THEN
+        RETURN 'Ocurrió un error inesperado.';
+END FUNCION_LIDER;
