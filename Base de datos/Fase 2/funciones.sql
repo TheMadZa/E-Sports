8424c86903e9c9acb2e88d3ffdd0c96961e6970a
Lorena-Arbe-Sanchez <126677607+Lorena-Arbe-Sanchez@users.noreply.github.com>
2024-05-03 09:04:43 +0200

    Update funciones.sql

--- Base de datos/Fase 2/funciones.sql
+++ Base de datos/Fase 2/funciones.sql
@@ -1,9 +1,11 @@
--- Función para ...
-CREATE OR REPLACE FUNCTION FUNCION_ ()
+/*
+Función para obtener el nombre del líder de un equipo, pasándole como
+parámetro el nombre del equipo.
+*/
+CREATE OR REPLACE FUNCTION FUNCION_LIDER ()
 RETURN X
 IS
 	;
 BEGIN
 	;
 END FUNCION_;
-
