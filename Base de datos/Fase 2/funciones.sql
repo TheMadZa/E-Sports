83ab45addd5ac8845f0794dbe988dd82fe545c5a
Lorena-Arbe-Sanchez <126677607+Lorena-Arbe-Sanchez@users.noreply.github.com>
2024-05-04 13:11:41 +0200

    Crear 2 funciones que se puedan utilizar en los triggers

--- Base de datos/Fase 2/funciones.sql
+++ Base de datos/Fase 2/funciones.sql
@@ -1,2 +1,37 @@
--- Función para que obtenga el resultado de un count de "JUGADOR"
---  desde el dato de entrada "ID_EQUIPO".
+/*
+Función para obtener la cantidad de jugadores de un equipo
+a partir del ID de ese equipo.
+Obtendrá el valor del resultado de un COUNT de "JUGADOR"
+en base al argumento "ID_EQUIPO".
+*/
+CREATE OR REPLACE FUNCTION OBTENER_CANTIDAD_JUGADORES(a_id_equipo IN EQUIPO.ID_EQUIPO%TYPE)
+RETURN NUMBER
+IS
+    v_cantidad_jugadores NUMBER(2);
+BEGIN
+    SELECT COUNT(*) INTO v_cantidad_jugadores
+	FROM JUGADOR
+	WHERE id_equipo = a_id_equipo;
+
+    RETURN v_cantidad_jugadores;
+END OBTENER_CANTIDAD_JUGADORES;
+
+/*
+Función para obtener el valor/estado de la etapa de una
+competición en la que participa un equipo, a partir del
+ID de ese equipo.
+Obtendrá el valor de la etapa en base al argumento "ID_EQUIPO".
+*/
+CREATE OR REPLACE FUNCTION OBTENER_ETAPA_COMPETICION(a_id_equipo IN EQUIPO.ID_EQUIPO%TYPE)
+RETURN VARCHAR2
+IS
+    v_etapa_competicion COMPETICION.ETAPA%TYPE;
+BEGIN
+    SELECT ETAPA INTO v_etapa_competicion
+	FROM COMPETICION
+	WHERE id_competicion = (SELECT id_competicion
+	                        FROM equipo_competicion
+	                        WHERE id_equipo = a_id_equipo);
+	
+    RETURN v_etapa_competicion;
+END OBTENER_ETAPA_COMPETICION;
