92088b7a8ca48769ee56a1a639307c8d3f1bc44b
IbaiSaenzDeBuruaga <126677217+IbaiSaenzDeBuruaga@users.noreply.github.com>
2024-05-07 13:35:41 +0200

    Update procedimientoClasificacion.sql

--- Base de datos/Fase 2/procedimientoClasificacion.sql
+++ Base de datos/Fase 2/procedimientoClasificacion.sql
@@ -1,20 +1,20 @@
-DROP PROCEDURE listarEquiposCompeticion;
-
-CREATE OR REPLACE PROCEDURE listarEquiposCompeticion
-(v_id_competicion IN COMPETICION.ID_COMPETICION%TYPE)
+CREATE OR REPLACE PROCEDURE listarEquiposCompeticion (v_id_competicion 
+                                    IN COMPETICION.ID_COMPETICION%TYPE)
 AS
     v_nombre_competicion COMPETICION.NOMBRE_COM%TYPE;
     v_id_equipo EQUIPO_COMPETICION.ID_EQUIPO%TYPE;
     v_nombre_equipo EQUIPO.NOM_EQUIPO%TYPE;
     v_victorias EQUIPO_COMPETICION.VICTORIAS%TYPE;
     v_puntos EQUIPO_COMPETICION.PUNTOS%TYPE;
+    v_juego VARCHAR2(50);
 BEGIN
-    FOR comp IN (SELECT C.NOMBRE_COM, EC.ID_EQUIPO, E.NOM_EQUIPO, 
-                    EC.VICTORIAS, EC.PUNTOS
+    FOR comp IN (SELECT C.NOMBRE_COM, EC.ID_EQUIPO, E.NOM_EQUIPO, EC.VICTORIAS, 
+                                            EC.PUNTOS, J.NOMBRE AS NOMBRE_JUEGO
                  FROM COMPETICION C
-                 INNER JOIN EQUIPO_COMPETICION EC 
-                 ON C.ID_COMPETICION = EC.ID_COMPETICION
+                 INNER JOIN EQUIPO_COMPETICION EC ON 
+                 C.ID_COMPETICION = EC.ID_COMPETICION
                  INNER JOIN EQUIPO E ON EC.ID_EQUIPO = E.ID_EQUIPO
+                 INNER JOIN JUEGO J ON C.ID_JUEGO = J.ID_JUEGO 
                  WHERE C.ID_COMPETICION = v_id_competicion
                  ORDER BY EC.VICTORIAS DESC, EC.PUNTOS DESC)
     LOOP
@@ -23,9 +23,10 @@ BEGIN
         v_nombre_equipo := comp.NOM_EQUIPO;
         v_victorias := comp.VICTORIAS;
         v_puntos := comp.PUNTOS;
-        
-        DBMS_OUTPUT.PUT_LINE('Competicion: ' || v_nombre_competicion 
-        || ', Equipo: ' || v_nombre_equipo || ', Victorias: ' 
-        || v_victorias || ', Puntos: ' || v_puntos);
+        v_juego := comp.NOMBRE_JUEGO; -- Asignar el nombre del juego
+
+        DBMS_OUTPUT.PUT_LINE('Competición: ' || v_nombre_competicion 
+        || ', Juego: ' || v_juego || ', Equipo: ' || v_nombre_equipo 
+        || ', Victorias: ' || v_victorias || ', Puntos: ' || v_puntos);
     END LOOP;
 END listarEquiposCompeticion;
