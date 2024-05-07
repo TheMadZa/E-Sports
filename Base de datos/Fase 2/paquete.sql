c02e73c87a75c79d77161dd979b0ad69bda2db39
Lorena-Arbe-Sanchez <126677607+Lorena-Arbe-Sanchez@users.noreply.github.com>
2024-05-07 13:55:33 +0200

    Update paquete.sql

--- Base de datos/Fase 2/paquete.sql
+++ Base de datos/Fase 2/paquete.sql
@@ -1,5 +1,7 @@
+-- Eliminar el paquete.
 DROP PACKAGE PAQUETE_DATOS;
 
+-- Crear la cabecera del paquete.
 CREATE OR REPLACE PACKAGE PAQUETE_DATOS
 IS
 	PROCEDURE OBTENER_INFO_COMPETICION(c_obtenerInfoCompeticion
@@ -12,6 +14,7 @@ IS
 END PAQUETE_DATOS;
 
 
+-- Crear el cuerpo del paquete.
 CREATE OR REPLACE PACKAGE BODY PAQUETE_DATOS
 AS
 	
@@ -83,18 +86,20 @@ AS
     PROCEDURE listarEquiposCompeticion(v_id_competicion IN
                                         COMPETICION.ID_COMPETICION%TYPE)
     AS
-        v_nombre_competicion COMPETICION.NOMBRE_COM%TYPE;
-        v_id_equipo EQUIPO_COMPETICION.ID_EQUIPO%TYPE;
-        v_nombre_equipo EQUIPO.NOM_EQUIPO%TYPE;
-        v_victorias EQUIPO_COMPETICION.VICTORIAS%TYPE;
-        v_puntos EQUIPO_COMPETICION.PUNTOS%TYPE;
+    v_nombre_competicion COMPETICION.NOMBRE_COM%TYPE;
+    v_id_equipo EQUIPO_COMPETICION.ID_EQUIPO%TYPE;
+    v_nombre_equipo EQUIPO.NOM_EQUIPO%TYPE;
+    v_victorias EQUIPO_COMPETICION.VICTORIAS%TYPE;
+    v_puntos EQUIPO_COMPETICION.PUNTOS%TYPE;
+    v_juego VARCHAR2(50);
     BEGIN
-        FOR comp IN (SELECT C.NOMBRE_COM, EC.ID_EQUIPO, E.NOM_EQUIPO, 
-                        EC.VICTORIAS, EC.PUNTOS
+        FOR comp IN (SELECT C.NOMBRE_COM, EC.ID_EQUIPO, E.NOM_EQUIPO, EC.VICTORIAS,
+                        EC.PUNTOS, J.NOMBRE AS NOMBRE_JUEGO
                      FROM COMPETICION C
-                     INNER JOIN EQUIPO_COMPETICION EC 
-                     ON C.ID_COMPETICION = EC.ID_COMPETICION
+                     INNER JOIN EQUIPO_COMPETICION EC ON
+                     C.ID_COMPETICION = EC.ID_COMPETICION
                      INNER JOIN EQUIPO E ON EC.ID_EQUIPO = E.ID_EQUIPO
+                     INNER JOIN JUEGO J ON C.ID_JUEGO = J.ID_JUEGO
                      WHERE C.ID_COMPETICION = v_id_competicion
                      ORDER BY EC.VICTORIAS DESC, EC.PUNTOS DESC)
         LOOP
@@ -103,10 +108,11 @@ AS
             v_nombre_equipo := comp.NOM_EQUIPO;
             v_victorias := comp.VICTORIAS;
             v_puntos := comp.PUNTOS;
-            
-            DBMS_OUTPUT.PUT_LINE('Competicion: ' || v_nombre_competicion 
-            || ', Equipo: ' || v_nombre_equipo || ', Victorias: ' 
-            || v_victorias || ', Puntos: ' || v_puntos);
+            v_juego := comp.NOMBRE_JUEGO; -- Asignar el nombre del juego
+    
+            DBMS_OUTPUT.PUT_LINE('Competición: ' || v_nombre_competicion
+            || ', Juego: ' || v_juego || ', Equipo: ' || v_nombre_equipo
+            || ', Victorias: ' || v_victorias || ', Puntos: ' || v_puntos);
         END LOOP;
     END listarEquiposCompeticion;
 
