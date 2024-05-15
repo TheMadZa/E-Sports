e33d906f0481b7abc697922c439751245eb91e05
Lorena <lorena.arbe@ikasle.egibide.org>
2024-05-15 12:24:42 +0200

    Ventana inicio sesión empezada.

---
+++ Programacion/RetoTheMadZa/src/main/java/Vista/VentanaInicioSesion.java
@@ -0,0 +1,10 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VentanaInicioSesion extends JFrame {
+    private JPanel pPrincipal;
+    private JTextField tfUsuario;
+    private JTextField tfContrasena;
+    private JButton iniciarSesiónButton;
+}
