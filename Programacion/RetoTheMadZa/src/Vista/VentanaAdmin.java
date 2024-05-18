d17739aab67acec2582f822f67886319bda292c2
Lorena <lorena.arbe@ikasle.egibide.org>
2024-05-18 13:33:03 +0200

    VAdmin adelantada un poco y optimizaciones.

--- Programacion/RetoTheMadZa/src/Vista/VentanaAdmin.java
+++ Programacion/RetoTheMadZa/src/Vista/VentanaAdmin.java
@@ -5,6 +5,7 @@ import org.imgscalr.Scalr;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -58,9 +59,23 @@ public class VentanaAdmin extends JFrame {
     private JPanel pBotones;
     private JPanel pDatos;
 
+    // Declarar la clase para colocar elementos que se le aplicará a los JLabel y JTextField en el pDatos.
+    private final GridBagConstraints gbc;
+
+    // Configurar el tipo de fuente general, tamaño de los JTextField y color que tendrán.
+    private final Font labelFont = new Font("Inter Semi Bold", Font.PLAIN, 26);
+    private final Dimension textFieldSize = new Dimension(350, 38);
+    private final Color labelColor = new Color(0xF9FBFF);
+
     public VentanaAdmin(){
 
         panelCRUD.setVisible(false);
+
+        // Configurar Layout del pDatos.
+        pDatos.setLayout(new GridBagLayout());
+        gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = new Insets(20, 20, 0, 20);
 
         // Cargar las imágenes con un tamaño específico.
         cargarImagenEstablecerIcono("TheMadZaLogoSimple", 250, 250, ftThemadza);
@@ -71,7 +86,7 @@ public class VentanaAdmin extends JFrame {
         cargarImagenEstablecerIcono("Instagram", bInstagram);
         cargarImagenEstablecerIcono("Facebook", bFacebook);
         cargarImagenEstablecerIcono("LogoBlanco", 100, 100, logoBlanco);
-        cargarImagenEstablecerIcono("LogoColor", 600, 600, tfImagenLogo);
+        cargarImagenEstablecerIcono("LogoColor", 650, 650, tfImagenLogo);
 
         // Configurar la ventana
         setContentPane(pPrincipal);
@@ -84,16 +99,6 @@ public class VentanaAdmin extends JFrame {
         for (JComponent componente : componentesConBorde) {
             componente.setBorder(BorderFactory.createEmptyBorder());
         }
-
-        /*
-        mPrincipal.setBorder(BorderFactory.createEmptyBorder());
-        bTienda.setBorder(BorderFactory.createEmptyBorder());
-        bInicio.setBorder(BorderFactory.createEmptyBorder());
-        bSalir.setBorder(BorderFactory.createEmptyBorder());
-        bFacebook.setBorder(BorderFactory.createEmptyBorder());
-        bTwitter.setBorder(BorderFactory.createEmptyBorder());
-        bInstagram.setBorder(BorderFactory.createEmptyBorder());
-         */
 
         setVisible(true);
     }
@@ -162,43 +167,31 @@ public class VentanaAdmin extends JFrame {
     }
 
     // Funciones
+
     public void mostrarDatosEquipos(){
 
-        // TODO : PONER BIEN TODO
-
-        setJlDato1("Nombre:");
-        JLabel jlFechaFundacion = new JLabel("Fecha de fundación:");
-        JTextField tfFechaFundacion = new JTextField(); //20
-        JLabel jlLogo = new JLabel("Logo:");
-        JTextField tfLogo = new JTextField();
-        JLabel jlColor = new JLabel("Color:");
-        JTextField tfColor = new JTextField();
-
-        pDatos.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        pDatos.add(getJlDato1(), gbc);
-        gbc.gridy++;
-        pDatos.add(jlFechaFundacion, gbc);
-        gbc.gridy++;
-        pDatos.add(tfFechaFundacion, gbc);
-        gbc.gridy++;
-        pDatos.add(jlLogo, gbc);
-        gbc.gridy++;
-        pDatos.add(tfLogo, gbc);
-        gbc.gridy++;
-        pDatos.add(jlColor, gbc);
-        gbc.gridy++;
-        pDatos.add(tfColor, gbc);
-
-        //pDatos.revalidate();
-        //pDatos.repaint();
+        // Agregar elementos al pDatos.
+        agregarElemento("Nombre:", new JTextField(), gbc);
+        agregarElemento("Fecha de fundación:", new JTextField(), gbc);
+        agregarElemento("Logo:", new JTextField(), gbc);
+        agregarElemento("Color:", new JTextField(), gbc);
 
     }
 
-    // Método auxiliar para cargar imágenes y establecer íconos (una función es para los JLabel y la otra para JButton)
+    public void mostrarDatosJugadores(){
+
+        // Agregar elementos al pDatos.
+        agregarElemento("Nombre:", new JTextField(), gbc);
+        agregarElemento("Nickname:", new JTextField(), gbc);
+        agregarElemento("Nacionalidad:", new JTextField(), gbc);
+        agregarElemento("Rol:", new JTextField(), gbc);
+        agregarElemento("Fecha de nacimiento:", new JTextField(), gbc);
+        agregarElemento("Sueldo:", new JTextField(), gbc);
+        agregarElemento("ID de su equipo:", new JTextField(), gbc);
+
+    }
+
+    // Función para cargar imágenes y establecer íconos (una función es para los JLabel y la otra para los JButton)
     private void cargarImagenEstablecerIcono(String nombreImagen, int ancho, int alto, JLabel label) {
         ImageIcon icono = ControladorImagenes.obtenerImagen(nombreImagen, ancho, alto);
         if (icono != null) {
@@ -214,5 +207,27 @@ public class VentanaAdmin extends JFrame {
         } else {
             System.err.println("La imagen " + nombreImagen + " no se encontró.");
         }
+    }
+
+    private void agregarElemento(String labelText, JTextField textField, GridBagConstraints gbc) {
+        // Configurar JLabel.
+        JLabel label = new JLabel(labelText);
+        label.setFont(labelFont);
+        label.setForeground(labelColor);
+
+        // Configurar JTextField.
+        textField.setPreferredSize(textFieldSize);
+        textField.setFont(labelFont);
+
+        // Agregar al panel.
+        gbc.gridx = 0;
+        gbc.gridy++;
+        pDatos.add(label, gbc);
+        gbc.gridx = 1;
+        pDatos.add(textField, gbc);
+
+        // Actualizar el panel
+        pDatos.revalidate();
+        pDatos.repaint();
     }
 }
